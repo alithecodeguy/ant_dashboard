c333b3c9571578b54595bc7510d098f7d8f0a1a2
faeze <77538218+faezemoradi8993@users.noreply.github.com>
2024-02-01 11:30:18 +0330

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <Button type="primary">Button</Button>
+        <Button type="primary">Button</Button
       </main>
     </>
   )
