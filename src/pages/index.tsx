da157075a2904b38a742b721ff1b3d6b6017cb02
faeze <77538218+faezemoradi8993@users.noreply.github.com>
2024-02-01 11:46:37 +0330

    build: husky , lint-staged installed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import { Button } from 'antd';
+import { Button } from 'antd'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <Button type="primary">Button</Button
+        <Button type="primary">Button</Button>
       </main>
     </>
   )
